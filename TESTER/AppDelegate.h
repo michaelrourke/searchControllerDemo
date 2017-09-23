8bb8e2f6234b5e5fd6d0c7d67e63f60952120ef1
Michael Rourke <michael@rourke.id.au>
2017-09-24 09:41:46 +1000

    Initial Commit

---
+++ TESTER/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TESTER
+//
+//  Created by Michael Rourke on 24/09/17.
+//  Copyright © 2017 Michael Rourke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TESTER/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TESTER
+//
+//  Created by Michael Rourke on 24/09/17.
+//  Copyright © 2017 Michael Rourke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
